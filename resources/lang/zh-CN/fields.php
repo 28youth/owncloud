6497460d0be3392061f3560d605ecaba027d0231
张卫 <daviezhang@vip.qq.com>
2019-01-18 16:24:41 +0800

    feat: 中文翻译文件

---
+++ resources/lang/zh-CN/fields.php
@@ -0,0 +1,22 @@
+<?php 
+
+return [
+
+	// 文件分类
+	'category' => [
+		'name' => '分类名称',
+		'symbol' => '分类编号',
+		'parent_id' => '父分类ID',
+		'is_system' => '是否系统分类',
+		'config_number' => '文件编号配置',
+		'config_number.*' => '文件编号配置',
+		'config_operate' => '文件操作配置',
+		'config_operate.*' => '文件操作配置',
+		'config_ability' => '文件权限配置',
+		'config_ability.*' => '文件权限配置',
+		'config_format' => '文件格式配置',
+		'config_format.*' => '文件格式配置',
+		'config_path' => '文件路径配置',
+		'description' => '分类备注',
+	],
+];
