d8f35830fa7e78747e42f997fba47378466942fb
张卫 <daviezhang@vip.qq.com>
2019-02-20 15:50:31 +0800

    ref： 重构文件分类权限表结构

--- database/migrations/2019_01_17_151935_create_role_has_categories_table.php
+++ database/migrations/2019_01_17_151935_create_role_has_categories_table.php
@@ -16,6 +16,12 @@ class CreateRoleHasCategoriesTable extends Migration
         Schema::create('role_has_categories', function (Blueprint $table) {
             $table->mediumInteger('role_id')->unsigned()->comment('角色ID');
             $table->mediumInteger('category_id')->unsigned()->comment('分类ID');
+            $table->unsignedTinyInteger('file_upload')->comment('文件上传权限');
+            $table->unsignedTinyInteger('file_delete')->comment('文件删除权限');
+            $table->unsignedTinyInteger('file_edit')->comment('文件编辑权限');
+            $table->unsignedTinyInteger('file_expired')->comment('文件过期时间权限');
+            $table->unsignedTinyInteger('file_edit_tag')->comment('标签编辑权限');
+            $table->unsignedTinyInteger('file_download')->comment('文件下载权限（&在线预览）');
 
             $table->foreign('role_id')->references('id')->on('roles')->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('category_id')->references('id')->on('categories')->onUpdate('cascade')->onDelete('cascade');
