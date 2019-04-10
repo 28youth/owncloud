9f1334431b8a4cc9dd9a02f12dd22407010a6a5f
28youth <daviezhang@vip.qq.com>
2019-04-10 16:29:53 +0800

    ref：文件详情返回操作权限

--- app/Http/Resources/FileResource.php
+++ app/Http/Resources/FileResource.php
@@ -21,7 +21,7 @@ class FileResource extends JsonResource
             'number' => $this->number,
             'origin_name' => $this->origin_name,
             'uploader' => $this->uploader,
-            // 'abilities' => $this->abilities,
+            'abilities' => $this->abilities,
             'download_sum' => $this->download_sum,
             'tags' => $this->tags,
             'category' => $this->category->only(['id', 'name', 'full_name']),
